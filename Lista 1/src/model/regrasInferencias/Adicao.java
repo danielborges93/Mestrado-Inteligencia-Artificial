2d0ed51e2ebb32c01f526c46f10d23cf06c39205
Núcleo de Excelência em Tecnologias Sociais <nees@nees-research1.nees.com.br>
2015-05-20 13:45:31 -0300

    Implementação da adição.

--- Lista 1/src/model/regrasInferencias/Adicao.java
+++ Lista 1/src/model/regrasInferencias/Adicao.java
@@ -1,6 +1,8 @@
 package model.regrasInferencias;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  *
@@ -10,7 +12,45 @@ public class Adicao extends Regra {
 
     @Override
     public boolean usar(List<String> proposicoes) {
-	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+	
+        // Recuperar todas os enunciados
+        Set<Character> enunciados = new HashSet<>();
+        
+        String proposicaoValida = null;
+        
+        // Percorrer as proposições e aproveitar para selecionar uma proposição válida
+        for(String proposicao : proposicoes) {
+            // Recuperar os operandos
+	    for (char c : proposicao.toCharArray()) {
+		/*
+		 * Verificar se o caractere está no intervalo [A-Z] e
+		 * é diferente de 'v'
+		 */
+		if (Character.isAlphabetic(c) && c != 'v') {
+		    enunciados.add(c);
+		}
+	    }
+            
+            // Verificar se a proposição é válida
+            if(proposicao.length() == 1) {
+                proposicaoValida = proposicao;
+            }
+        }
+        
+        // Se encontrou uma proposição válida...
+        if(proposicaoValida != null) {
+            // Remover a proposição que vai ser utilizada do Set
+            enunciados.remove(proposicaoValida.charAt(0));
+            
+            // Remover a proposição válida da lista de proposições e adicionar a nova
+            proposicoes.remove(proposicaoValida);
+            String nova = proposicaoValida + "v" + enunciados.toArray()[0];
+            proposicoes.add(nova);
+            
+            return true;
+        }
+        
+        return false;
     }
     
 }
