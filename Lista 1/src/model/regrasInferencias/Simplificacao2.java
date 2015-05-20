b38d5108341fe21ef556851354c2a8facd193458
Núcleo de Excelência em Tecnologias Sociais <nees@nees-research1.nees.com.br>
2015-05-20 14:12:06 -0300

    Implementação de todas as regras de inferência.

---
+++ Lista 1/src/model/regrasInferencias/Simplificacao2.java
@@ -0,0 +1,16 @@
+package model.regrasInferencias;
+
+import java.util.List;
+
+/**
+ *
+ * @author danielborges93
+ */
+public class Simplificacao extends Regra {
+
+    @Override
+    public boolean usar(List<String> proposicoes) {
+	throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
