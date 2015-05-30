5053c9a99c04fbbe79638542d4807c76297e64d5
Daniel Borges Ferreira da Silva <daniel_borges_93@yahoo.com.br>
2015-05-30 18:21:05 -0300

    Casos complexos na regra Modus Tollens.

--- src/model/regrasInferencias/ModusTollens.java
+++ src/model/regrasInferencias/ModusTollens.java
@@ -12,55 +12,53 @@ import model.Expressao;
 public class ModusTollens extends Regra {
 
     public ModusTollens() {
-//	this.entrada1 = "^([A-Z])>([A-Z])$";
 	this.nome = "Modus Tollens";
     }
 
     @Override
     public boolean usar(List<Expressao> proposicoes) {
 
-//	// Declarar as duas entradas
-//	String[] input = new String[2];
-//
-//	// Boolean que diz se encontrou ou não
-//	boolean[] encontrou = {false, false};
-//
-//	// Percorrer as proposições
-//	for (String proposicao : proposicoes) {
-//	    // Tentar encontrar o padrão
-//	    Pattern pattern = Pattern.compile(this.entrada1);
-//	    Matcher matcher = pattern.matcher(proposicao);
-//
-//	    // Se encontrar
-//	    if (matcher.find()) {
-//		// Salvar as entradas
-//		input[0] = proposicao;
-//		input[1] = "¬" + matcher.group(2);
-//		this.saida = "¬" + matcher.group(1);
-//
-//		encontrou[0] = true;
-//
-//		// Para o 'for'
-//		break;
-//	    }
-//
-//	}
-//
-//	// Tenta achar a segunda entrada
-//	encontrou[1] = proposicoes.contains(input[1]);
-//
-//	// Se encontrou as duas entradas
-//	if (encontrou[0] && encontrou[1]) {
-//	    
-//	    // Remove as entradas da lista
-//	    proposicoes.remove(input[0]);
-//	    proposicoes.remove(input[1]);
-//
-//	    // Salva a saída
-//	    proposicoes.add(this.saida);
-//
-//	    return true;
-//	}
+	// Boolean que diz se encontrou ou não
+	boolean[] encontrou = {false, false};
+
+	// Percorrer as proposições
+	for (Expressao proposicao : proposicoes) {
+	    // Tentar encontrar o padrão
+	    Pattern pattern = Pattern.compile("^(¬?.+)>(¬?.+)$");
+	    Matcher matcher = pattern.matcher(proposicao.getInfixa());
+
+	    // Se encontrar
+	    if (matcher.find()) {
+		// Salvar as entradas
+		this.entrada1 = proposicao;
+		this.entrada2 = new Expressao("¬" + matcher.group(2), null);
+		
+		// Salvar a saída
+		this.saida = new Expressao("¬" + matcher.group(1), null);
+
+		encontrou[0] = true;
+
+		// Para o 'for'
+		break;
+	    }
+
+	}
+
+	// Tenta achar a segunda entrada
+	encontrou[1] = proposicoes.contains(this.entrada2);
+
+	// Se encontrou as duas entradas
+	if (encontrou[0] && encontrou[1]) {
+	    
+	    // Remove as entradas da lista
+	    proposicoes.remove(this.entrada1);
+	    proposicoes.remove(this.entrada2);
+
+	    // Salva a saída
+	    proposicoes.add(this.saida);
+
+	    return true;
+	}
 
 	return false;
     }
