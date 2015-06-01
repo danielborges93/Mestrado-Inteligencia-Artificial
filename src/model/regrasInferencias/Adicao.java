d0256d42d12e3c2c7a79c6ede0402d991ed11f0e
Daniel Borges Ferreira da Silva <daniel_borges_93@yahoo.com.br>
2015-06-01 12:51:21 -0300

    Casos complexos na regra Adição #10

--- src/model/regrasInferencias/Adicao.java
+++ src/model/regrasInferencias/Adicao.java
@@ -18,42 +18,42 @@ public class Adicao extends Regra {
     @Override
     public boolean usar(List<Expressao> proposicoes) {
 
-//	// Recuperar todas os enunciados
-//	Set<Character> enunciados = new HashSet<>();
-//
-//	String proposicaoValida = null;
-//
-//	// Percorrer as proposições e aproveitar para selecionar uma proposição válida
-//	for (String proposicao : proposicoes) {
-//	    // Recuperar os operandos
-//	    for (char c : proposicao.toCharArray()) {
-//		/*
-//		 * Verificar se o caractere está no intervalo [A-Z] e
-//		 * é diferente de 'v'
-//		 */
-//		if (Character.isAlphabetic(c) && c != 'v') {
-//		    enunciados.add(c);
-//		}
-//	    }
-//
-//	    // Verificar se a proposição é válida
-//	    if (proposicao.length() == 1) {
-//		proposicaoValida = proposicao;
-//	    }
-//	}
-//
-//	// Se encontrou uma proposição válida...
-//	if (proposicaoValida != null && enunciados.size() > 1) {
-//	    // Remover a proposição que vai ser utilizada do Set
-//	    enunciados.remove(proposicaoValida.charAt(0));
-//
-//	    // Remover a proposição válida da lista de proposições e adicionar a nova
-//	    proposicoes.remove(proposicaoValida);
-//	    String nova = proposicaoValida + "v" + enunciados.toArray()[0];
-//	    proposicoes.add(nova);
-//
-//	    return true;
-//	}
+	// Recuperar todas os enunciados
+	Set<Character> enunciados = new HashSet<>();
+
+	// Percorrer as proposições e aproveitar para selecionar uma proposição válida
+	for (Expressao proposicao : proposicoes) {
+	    // Recuperar os operandos
+	    for (char c : proposicao.getPosfixa().toCharArray()) {
+		/*
+		 * Verificar se o caractere está no intervalo [A-Z] e
+		 * é diferente de 'v'
+		 */
+		if (Character.isAlphabetic(c) && c != 'v') {
+		    enunciados.add(c);
+		}
+	    }
+
+	    // Verificar se a proposição é válida
+	    if (proposicao.getInfixa().length() == 1) {
+		this.entrada1 = proposicao;
+	    }
+	}
+
+	// Se encontrou uma proposição válida...
+	if (this.entrada1 != null && enunciados.size() > 1) {
+	    // Remover a proposição que vai ser utilizada do Set
+	    enunciados.remove(this.entrada1.getInfixa().charAt(0));
+
+	    // Remover a proposição válida da lista de proposições e adicionar a nova
+	    proposicoes.remove(this.entrada1);
+	    
+	    String novaString = this.entrada1 + "v" + enunciados.toArray()[0];
+	    Expressao novaExpressao = new Expressao(novaString, null);
+	    proposicoes.add(novaExpressao);
+
+	    return true;
+	}
 
 	return false;
     }
